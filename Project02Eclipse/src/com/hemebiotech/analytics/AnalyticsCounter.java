15806c6d9a145bbb5e19ce63c109484d0b5d44ee
Vincent Viomesnil <vincent.viomesnil@axa.fr>
2022-02-08 17:05:15 +0100

    Javadoc OK

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -3,36 +3,29 @@ package com.hemebiotech.analytics;
 import java.util.List;
 import java.util.TreeMap;
 
-/** 
+/**
  * @author Vincent Viomesnil
  */
 
 public class AnalyticsCounter {
 
 	static final String inputFile = ".\\Project02Eclipse\\symptoms.txt";
-	static final String outputFile = "result.out"; 
+	static final String outputFile = ".\\Project02Eclipse\\result.out";
+	static TreeMap<String, Integer> mapOccurrence = new TreeMap<String, Integer>();
 
-/**
- * 
- * @param args an array of command-line arguments for the application.
- * @throws Exception with the reading or writing of Input/Output Stream
- */
+	/**
+	 * @param args an array of command-line arguments for the application.
+	 * @throws Exception with the reading or writing of Input/Output Stream
+	 */
 	public static void main(String[] args) throws Exception {
-		
-/**
- * @param mapOccurrence Creation of the symptoms list by occurrence.
- */
 
-		ISymptomReader reader = new ReadSymptomDataFromFile(inputFile); // Read the file that contains the symptoms list. 
-		List<String> symptomsList = reader.getSymptoms();  
-		TreeMap<String, Integer> mapOccurrence = new TreeMap<String, Integer>(); // instantiation of the map "mapOccurrence".   	
+		ISymptomReader reader = new ReadSymptomDataFromFile(inputFile);
+		List<String> symptomsList = reader.getSymptoms();
 		
-		CountSymptoms countSymptoms = new CountSymptoms(); // Creation of the object "countSymptoms" of the "CountSymptoms" class
-		mapOccurrence = countSymptoms.nbOccurrence(symptomsList);  // "mapOccurrence" that returns the list of symptoms and display the number of occurrences
+		CountSymptoms countSymptoms = new CountSymptoms();
+		mapOccurrence = countSymptoms.nbOccurrence(symptomsList);
 		
-		System.out.println(mapOccurrence); // Display the number of occurrences from the map 
-		
-		ISymptomWriter writer = new WriteSymptomOnFile(outputFile); // Write on the outputFile
-		writer.writeSymptoms(mapOccurrence); // Writing the occurrence per symptom in the output file "result.out" 		
-		}
+		ISymptomWriter writer = new WriteSymptomOnFile(outputFile);
+		writer.writeSymptoms(mapOccurrence);
+	}
 }
--- Project02Eclipse/src/com/hemebiotech/analytics/WriteSymptomOnFile.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/WriteSymptomOnFile.java
@@ -1,41 +1,32 @@
 package com.hemebiotech.analytics;
 
-
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Map.Entry;
 import java.util.TreeMap;
 
-/**
- * 
- * @param writes in a output file the list of symptoms sorted in alphabetical order
- */
-
 public class WriteSymptomOnFile implements ISymptomWriter {
 
 	private String outputFile;
-	
 
 	public WriteSymptomOnFile(String outputFile) throws IOException {
 		this.outputFile = outputFile;
 	}
-	
+
 	@Override
-	public void writeSymptoms (TreeMap<String, Integer> nbOccurrence) throws IOException{ //Write the result in a file, after counting and sorting from CountSymptoms class
-			
-		 try (FileWriter fileWriter = new FileWriter(outputFile, false);
-				BufferedWriter writer = new BufferedWriter (fileWriter)) {
-			 for (Entry<String, Integer> entry : nbOccurrence.entrySet()) {
-					writer.write(entry.getKey()+" = "+entry.getValue());
-					writer.newLine();
-				}
-				writer.close();
+	public void writeSymptoms(TreeMap<String, Integer> nbOccurrence) throws IOException {
 
-			}catch (IOException e) {
-				e.printStackTrace();
+		try (FileWriter fileWriter = new FileWriter(outputFile, false);
+				BufferedWriter writer = new BufferedWriter(fileWriter)) {
+			for (Entry<String, Integer> entry : nbOccurrence.entrySet()) {
+				writer.write(entry.getKey() + " = " + entry.getValue());
+				writer.newLine();
 			}
+			writer.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
+	}
 }
-
-
